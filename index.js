87a1ee538aede502dfa1e1c660bb764cbd119dba
Barnabas Nduwak Suku <barnssuku@gmail.com>
2019-01-07 06:33:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  var selectorSelected = document.querySelectorAll(selector)[0];
+  return selectorSelected;
+}
 
+function nestedTarget() {
+  var elem = document.querySelector('#nested .target');
+  return elem;
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll('.ranked-list li');
+  for (var i = 0; i < rankedList.length; i++) {
+    linkValue = parseInt(rankedList[i], 10);
+    linkValue += n;
+    rankedList[i].innerHTML = linkValue;
+  }
+}
+
+function deepestChild() {
+  var deepestChild = document.querySelector('div#grand-node');
+  
+}
